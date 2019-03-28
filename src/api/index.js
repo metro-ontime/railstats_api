e44d4ae2b0ec582d36d93e41b3e9d2e0e4ccc29e
brianplynn <brnlynn889@gmail.com>
2019-03-28 16:00:42 -0700

    added history and line history paths

--- src/api/index.js
+++ src/api/index.js
@@ -2,12 +2,16 @@ import { version } from '../../package.json';
 import { Router } from 'express';
 import line from './line';
 import network from './network';
+import history from "./history"
+import linehistory from "./linehistory"
 
 export default ({ config, db }) => {
 	let api = Router();
 
 	api.use('/line', line({ config, db }));
 	api.use('/network', network({ config, db }));
+	api.use('/history', history({ config, db }));
+	api.use('/linehistory', linehistory({ config, db }));
 
 	// perhaps expose some API metadata at the root
 	api.get('/', (req, res) => {
---
+++ src/api/linehistory.js
@@ -0,0 +1,30 @@
+import resource from 'resource-router-middleware';
+
+const lines = ['801', '802', '803', '804', '805', '806'];
+
+export default ({ config, db }) => resource({
+
+	/** Property name to store preloaded entity on `request`. */
+	id : 'linehistory',
+
+	/** For requests with an `id`, you can auto-load the entity.
+	 *  Errors terminate the request, success sets `req[id] = data`.
+	 */
+	load(req, id, callback) {
+		let line = lines.find( line => line===id ),
+			err = line ? null : { "line": "Not found", "data": {} };
+		callback(err, line);
+	},
+
+	/** GET / - List all entities */
+	index({ params }, res) {
+    res.json({ "lines": lines });
+	},
+
+	/** GET /:id - Return a given entity */
+	read({ linehistory }, res) {
+    db.getLineHistory(linehistory).then((data) => {
+      res.json({ ...data });
+    });
+	},
+});
